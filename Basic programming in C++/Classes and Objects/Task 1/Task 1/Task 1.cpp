f3fd43ac591f094d945a6aa9799c501f13243b7b
Morgan <nikitakut3000@gmail.com>
2024-07-29 16:51:26 +0300

    Classes and Objects

---
+++ Basic programming in C++/Classes and Objects/Task 1/Task 1/Task 1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+class Calculator {
+private:
+    double num_1, num_2;
+public:
+    double add() {
+        return num_1 + num_2;
+    }
+    double multiply() {
+        return num_1 * num_2;
+    }
+    double substract_1_2() {
+        return num_2 - num_1;
+    }
+    double substract_2_1() {
+        return num_1 - num_2;
+    }
+    double divide_1_2() {
+        return num_2 / num_1;
+    }
+    double divide_2_1() {
+        return num_1 / num_2;
+    }
+    bool set_num_1(double num_1) {
+        if (!num_1) return false;
+        this->num_1 = num_1;
+        return true;
+    }
+    bool set_num_2(double num_2) {
+        if (!num_2) {
+            std::cout << "Incorrect input!" << std::endl;
+            return false;
+        };
+        this->num_2 = num_2;
+        return true;
+    }
+};
+
+int main()
+{
+    Calculator calculator;
+    double num_1, num_2;
+    while (true) {
+        while (true)
+        {
+            std::cout << "Enter num1: ";
+            std::cin >> num_1;
+            if (num_1) {
+                calculator.set_num_1(num_1);
+                break;
+            }
+            std::cout << "Incorrect Input!" << std::endl;
+        }
+
+        while (true)
+        {
+            std::cout << "Enter num2: ";
+            std::cin >> num_2;
+            if (num_2) {
+                calculator.set_num_2(num_2);
+                break;
+            }
+            std::cout << "Incorrect Input!" << std::endl;
+        }
+
+        std::cout << "num1 + num2 = " << calculator.add() << std::endl;
+        std::cout << "num1 - num2 = " << calculator.substract_2_1() << std::endl;
+        std::cout << "num2 - num1 = " << calculator.substract_1_2() << std::endl;
+        std::cout << "num1 * num2 = " << calculator.multiply() << std::endl;
+        std::cout << "num1 / num2 = " << calculator.divide_2_1() << std::endl;
+        std::cout << "num2 / num1 = " << calculator.divide_1_2() << std::endl;
+    }
+};
+   
---
+++ Basic programming in C++/Classes and Objects/Task 2/Task 2/Task 2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+class Counter {
+private:
+    int value = 1;
+public:
+    void set_initial_value(int value) {
+        this->value = value;
+    }
+    void increase_value() {
+        value++;
+    }
+    void decrease_value() {
+        value--;
+    }
+    void show_value() {
+        std::cout << value << std::endl;
+    }
+};
+
+int main()
+{
+    Counter counter;
+    for (int i = 0; i < 4; i++)
+    {
+        char is_initial_value = ' ';
+        std::cout << "Do you want to set the initial value? (yes - y | no - n): ";
+        std::cin >> is_initial_value;
+        if (is_initial_value == 'y') {
+            int initial_value;
+            std::cout << "Enter the initial value: ";
+            std::cin >> initial_value;
+            counter.set_initial_value(initial_value);
+            break;
+        }
+        else {
+            std::cout << "Incorrect Input!" << std::endl;
+        }
+    }
+
+
+    char command;
+    do
+    {
+        std::cout << "Enter the command ('+', '-', '=' or 'x'): ";
+        std::cin >> command;
+        if (command == '+')
+        {
+            counter.increase_value();
+        }
+        else if (command == '-') {
+            counter.decrease_value();
+        }
+        else if (command == '=') {
+            counter.show_value();
+        }
+    } while (command != 'x');
+    std::cout << "Goodbye!" << std::endl;
+}
